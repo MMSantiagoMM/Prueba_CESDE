f1acbb0426ca675ff5e42e5555f773dc2e5cb719
Mapura <mapura@kali.168.1.7>
2025-02-24 10:22:23 -0500

    Starting project

---
+++ src/main/java/com/example/pruebaCesde/PruebaCesdeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebaCesde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCesdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCesdeApplication.class, args);
+	}
+
+}
